281a10dd9f20c20dd2170584037f299ebb4b2171
Jens Reimann <jens.reimann@ibh-systems.com>
2013-10-09 18:40:05 +0200

    update license header

--- org.eclipse.scada.utils.stats/src/org/eclipse/scada/utils/stats/StatisticEntry.java
+++ org.eclipse.scada.utils.stats/src/org/eclipse/scada/utils/stats/StatisticEntry.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2012 TH4 SYSTEMS GmbH and others.
+ * Copyright (c) 2006, 2013 TH4 SYSTEMS GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     TH4 SYSTEMS GmbH - initial API and implementation
+ *     IBH SYSTEMS GmbH - refactor for new repository layout
  *******************************************************************************/
 package org.eclipse.scada.utils.stats;
 
