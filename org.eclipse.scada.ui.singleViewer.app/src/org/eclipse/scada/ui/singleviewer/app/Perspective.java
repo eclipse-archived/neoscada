ca73eec7bf06182f888ab23a6d15effd421ec450
Jens Reimann <jens.reimann@ibh-systems.com>
2013-10-11 17:50:10 +0200

    initial checkin of the ESAC application

--- org.eclipse.scada.ui.singleViewer.app/src/org/eclipse/scada/ui/singleviewer/app/Perspective.java
+++ org.eclipse.scada.ui.singleViewer.app/src/org/eclipse/scada/ui/singleviewer/app/Perspective.java
@@ -15,7 +15,6 @@ import org.eclipse.ui.IPerspectiveFactory;
 
 public class Perspective implements IPerspectiveFactory
 {
-
     @Override
     public void createInitialLayout ( final IPageLayout layout )
     {
