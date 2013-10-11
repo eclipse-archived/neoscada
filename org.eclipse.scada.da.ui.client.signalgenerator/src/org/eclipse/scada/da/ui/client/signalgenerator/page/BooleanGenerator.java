ceff60ce60a483c9b11ec1d04270b02b8cf35824
Jens Reimann <jens.reimann@ibh-systems.com>
2013-10-11 10:52:02 +0200

    adding modules to the build

---
+++ org.eclipse.scada.da.ui.client.signalgenerator/src/org/eclipse/scada/da/ui/client/signalgenerator/page/BooleanGenerator.java
@@ -0,0 +1,152 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2013 TH4 SYSTEMS GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     TH4 SYSTEMS GmbH - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.scada.da.ui.client.signalgenerator.page;
+
+import org.eclipse.scada.core.Variant;
+import org.eclipse.scada.da.ui.client.signalgenerator.SimulationTarget;
+import org.eclipse.swt.widgets.Display;
+
+public class BooleanGenerator
+{
+    public enum State
+    {
+        START_DELAY,
+        END_DELAY,
+    }
+
+    private Display display;
+
+    private int startDelay = 1000;
+
+    private int endDelay = 1000;
+
+    private int iterations = -1;
+
+    private boolean running = false;
+
+    private long lastTick = 0;
+
+    private State currentState;
+
+    private final SimulationTarget target;
+
+    public BooleanGenerator ( final Display display, final SimulationTarget target )
+    {
+        this.display = display;
+        this.target = target;
+    }
+
+    public void start ()
+    {
+        if ( this.running )
+        {
+            return;
+        }
+        this.lastTick = System.currentTimeMillis ();
+        this.currentState = State.START_DELAY;
+        this.running = true;
+        triggerRun ();
+    }
+
+    private void triggerRun ()
+    {
+        this.display.timerExec ( 100, new Runnable () {
+
+            @Override
+            public void run ()
+            {
+                if ( BooleanGenerator.this.running )
+                {
+                    BooleanGenerator.this.tick ();
+                    BooleanGenerator.this.triggerRun ();
+                }
+            }
+        } );
+    }
+
+    public void stop ()
+    {
+        if ( !this.running )
+        {
+            this.running = false;
+            return;
+        }
+    }
+
+    public void dispose ()
+    {
+        stop ();
+        this.display = null;
+    }
+
+    protected void tick ()
+    {
+        final long now = System.currentTimeMillis ();
+        switch ( this.currentState )
+        {
+        case START_DELAY:
+            if ( now - this.lastTick > this.startDelay )
+            {
+                this.currentState = State.END_DELAY;
+                this.lastTick = now;
+                this.target.writeValue ( Variant.TRUE );
+            }
+            break;
+        case END_DELAY:
+            if ( now - this.lastTick > this.endDelay )
+            {
+                this.currentState = State.START_DELAY;
+                this.lastTick = now;
+                if ( this.iterations > 0 )
+                {
+                    this.iterations--;
+                }
+                this.target.writeValue ( Variant.FALSE );
+            }
+            break;
+        }
+
+        if ( this.iterations == 0 )
+        {
+            this.running = false;
+        }
+    }
+
+    public int getStartDelay ()
+    {
+        return this.startDelay;
+    }
+
+    public void setStartDelay ( final int startDelay )
+    {
+        this.startDelay = startDelay;
+    }
+
+    public int getEndDelay ()
+    {
+        return this.endDelay;
+    }
+
+    public void setEndDelay ( final int endDelay )
+    {
+        this.endDelay = endDelay;
+    }
+
+    public int getIterations ()
+    {
+        return this.iterations;
+    }
+
+    public void setIterations ( final int iterations )
+    {
+        this.iterations = iterations;
+    }
+}
---
+++ org.eclipse.scada.da.ui.client.test/src/org/eclipse/scada/da/client/test/wizards/WriteOperationWizardValuePage.java
@@ -0,0 +1,241 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2013 TH4 SYSTEMS GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     TH4 SYSTEMS GmbH - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.scada.da.client.test.wizards;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.IWizardPage;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.scada.core.NotConvertableException;
+import org.eclipse.scada.core.NullValueException;
+import org.eclipse.scada.core.Variant;
+import org.eclipse.scada.da.ui.common.ValueType;
+import org.eclipse.scada.da.ui.connection.data.Item;
+import org.eclipse.scada.da.ui.connection.data.ItemSelectionHelper;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class WriteOperationWizardValuePage extends WizardPage implements IWizardPage
+{
+
+    private final static Logger logger = LoggerFactory.getLogger ( WriteOperationWizardValuePage.class );
+
+    private Text itemIdText;
+
+    private Text valueText;
+
+    private Combo valueTypeSelect;
+
+    private Text convertedValue;
+
+    private Color defaultValueColor;
+
+    private Variant value;
+
+    private Item item;
+
+    protected WriteOperationWizardValuePage ()
+    {
+        super ( "wizardPage" );
+        setTitle ( "Write Data Item" );
+        setDescription ( "Enter the information to write" );
+    }
+
+    @Override
+    public void createControl ( final Composite parent )
+    {
+        final Composite container = new Composite ( parent, SWT.NONE );
+
+        final GridLayout layout = new GridLayout ();
+        container.setLayout ( layout );
+        layout.numColumns = 3;
+        layout.verticalSpacing = 9;
+
+        Label label = new Label ( container, SWT.NONE );
+        label.setText ( "&Item:" );
+
+        this.itemIdText = new Text ( container, SWT.BORDER | SWT.SINGLE );
+        GridData gd = new GridData ( GridData.FILL_HORIZONTAL );
+        this.itemIdText.setLayoutData ( gd );
+        this.itemIdText.addModifyListener ( new ModifyListener () {
+            @Override
+            public void modifyText ( final ModifyEvent e )
+            {
+                dialogChanged ();
+            }
+        } );
+
+        label = new Label ( container, SWT.NONE );
+
+        // row 2
+        label = new Label ( container, SWT.NONE );
+        label.setText ( "&Value:" );
+        label.setLayoutData ( new GridData ( SWT.BEGINNING, SWT.BEGINNING, false, false ) );
+
+        this.valueText = new Text ( container, SWT.BORDER | SWT.MULTI );
+        gd = new GridData ( SWT.FILL, SWT.FILL, true, true );
+        this.valueText.setLayoutData ( gd );
+        this.valueText.addModifyListener ( new ModifyListener () {
+            @Override
+            public void modifyText ( final ModifyEvent e )
+            {
+                dialogChanged ();
+            }
+        } );
+
+        this.valueTypeSelect = new Combo ( container, SWT.DROP_DOWN | SWT.READ_ONLY );
+        for ( final ValueType vt : ValueType.values () )
+        {
+            this.valueTypeSelect.add ( vt.label (), vt.ordinal () );
+        }
+        this.valueTypeSelect.addSelectionListener ( new SelectionAdapter () {
+            @Override
+            public void widgetSelected ( final SelectionEvent e )
+            {
+                dialogChanged ();
+            }
+        } );
+        this.valueTypeSelect.select ( ValueType.STRING.ordinal () );
+        this.valueTypeSelect.setLayoutData ( new GridData ( SWT.BEGINNING, SWT.BEGINNING, false, false ) );
+
+        // row 3
+
+        label = new Label ( container, SWT.NONE );
+        label.setText ( "Converted Value: " );
+
+        this.convertedValue = new Text ( container, SWT.SINGLE | SWT.READ_ONLY | SWT.BORDER );
+        gd = new GridData ( GridData.FILL_HORIZONTAL );
+        this.convertedValue.setLayoutData ( gd );
+        this.defaultValueColor = this.convertedValue.getForeground ();
+
+        setControl ( container );
+        updateSelection ();
+        dialogChanged ();
+    }
+
+    private void updateSelection ()
+    {
+        if ( this.item != null )
+        {
+            this.itemIdText.setText ( this.item.getId () );
+        }
+        else
+        {
+            this.itemIdText.setText ( "" );
+        }
+    }
+
+    private void setValueText ( final String value, final boolean systemText )
+    {
+        this.convertedValue.setText ( value );
+
+        if ( systemText )
+        {
+            final Color color = this.convertedValue.getDisplay ().getSystemColor ( SWT.COLOR_RED );
+            this.convertedValue.setForeground ( color );
+        }
+        else
+        {
+            this.convertedValue.setForeground ( this.defaultValueColor );
+        }
+    }
+
+    private void dialogChanged ()
+    {
+        // connection
+        if ( this.item == null )
+        {
+            updateStatus ( "No item selection" );
+            return;
+        }
+
+        // item
+        if ( this.itemIdText.getText ().length () <= 0 )
+        {
+            updateStatus ( "Item name must not be empty" );
+            return;
+        }
+
+        // value stuff
+        setValueText ( "<not set>", true );
+        this.value = null;
+
+        final int idx = this.valueTypeSelect.getSelectionIndex ();
+        try
+        {
+            for ( final ValueType vt : ValueType.values () )
+            {
+                if ( vt.ordinal () == idx )
+                {
+                    this.value = vt.convertTo ( this.valueText.getText () );
+                }
+            }
+        }
+        catch ( final NotConvertableException e )
+        {
+            updateStatus ( "Unable to convert value to target type: " + e.getMessage () );
+            return;
+        }
+        catch ( final Exception e )
+        {
+            logger.error ( "Failed to convert", e );
+        }
+        if ( this.value != null )
+        {
+            try
+            {
+                setValueText ( this.value.asString (), false );
+            }
+            catch ( final NullValueException e )
+            {
+                setValueText ( "<null>", true );
+            }
+        }
+        else
+        {
+            setValueText ( "no converter found for: " + idx, true );
+        }
+
+        updateStatus ( null );
+    }
+
+    private void updateStatus ( final String message )
+    {
+        setErrorMessage ( message );
+        setPageComplete ( message == null );
+    }
+
+    public Item getItem ()
+    {
+        return new Item ( this.item.getConnectionString (), this.itemIdText.getText (), this.item.getType () );
+    }
+
+    public Variant getValue ()
+    {
+        return this.value;
+    }
+
+    public void setSelection ( final IStructuredSelection selection )
+    {
+        this.item = ItemSelectionHelper.getFirstFromSelection ( selection );
+    }
+}
