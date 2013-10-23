6ed8d42818623e687b62faeec6ddd8bfc160eb8b
Jens Reimann <jens.reimann@ibh-systems.com>
2013-10-23 12:10:44 +0200

    add conversion of RGB to string

--- org.eclipse.scada.ui.chart.model/emfsrc/org/eclipse/scada/ui/chart/model/impl/ChartFactoryImpl.java
+++ org.eclipse.scada.ui.chart.model/emfsrc/org/eclipse/scada/ui/chart/model/impl/ChartFactoryImpl.java
@@ -491,11 +491,18 @@ public class ChartFactoryImpl extends EFactoryImpl implements ChartFactory
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
      * 
-     * @generated
+     * @generated NOT
      */
     public String convertRGBToString ( final EDataType eDataType, final Object instanceValue )
     {
-        return super.convertToString ( eDataType, instanceValue );
+        final RGB rgb = (RGB)instanceValue;
+
+        if ( rgb == null )
+        {
+            return null;
+        }
+
+        return String.format ( "#%02X%02X%02X", rgb.red, rgb.green, rgb.blue ); //$NON-NLS-1$
     }
 
     /**
