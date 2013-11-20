1fbc1678e735793129d87ec1f5359d13a51fa049
Jens Reimann <jens.reimann@ibh-systems.com>
2013-11-20 13:00:43 +0100

    different output director

--- org.eclipse.scada.deploy.packages/post.sh
+++ org.eclipse.scada.deploy.packages/post.sh
@@ -2,15 +2,19 @@
 
 set -e
 
-rm -Rf ../output
-mkdir ../output
-for i in `find /tmp/eclipse.scada.rpmbuild -name "*.rpm"`; do
- cp -v "$i" ../output/
-done
-find . -name "*.deb" -exec cp -v {} ../output/ \;
-find . -name "*.changes" -exec cp -v {} ../output/ \;
+OUTPUT="rpm.output"
 
-pushd ../output
+echo "Output: $OUTPUT"
+
+rm -Rf "$OUTPUT"
+mkdir "$OUTPUT"
+for i in `find /tmp/eclipse.scada.rpmbuild -name "*.rpm"`; do
+ cp -v "$i" "$OUTPUT"
+done
+find . -name "*.deb" -exec cp -v {} "$OUTPUT" \;
+find . -name "*.changes" -exec cp -v {} "$OUTPUT" \;
+
+pushd "$OUTPUT"
 v=`ls org.eclipse.scada-p2*.rpm | sed -s 's/^org.eclipse.scada-p2-\(.*\).noarch.rpm$/\1/'`
 echo Version: $v
 echo org.eclipse.scada.rpmFileName=org.eclipse.scada-${v}.noarch.rpm > rpm.properties
