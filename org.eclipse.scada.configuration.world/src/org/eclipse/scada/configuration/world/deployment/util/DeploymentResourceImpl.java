4849ce5097aa7bb7c5023580a6588f58bb6590fb
Jens Reimann <jens.reimann@ibh-systems.com>
2014-01-08 10:23:39 +0100

    use UUIDs

--- org.eclipse.scada.configuration.world/src/org/eclipse/scada/configuration/world/deployment/util/DeploymentResourceImpl.java
+++ org.eclipse.scada.configuration.world/src/org/eclipse/scada/configuration/world/deployment/util/DeploymentResourceImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 IBH SYSTEMS GmbH and others.
+ * Copyright (c) 2013, 2014 IBH SYSTEMS GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,13 +11,13 @@
 package org.eclipse.scada.configuration.world.deployment.util;
 
 import org.eclipse.emf.common.util.URI;
-
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
 
 /**
  * <!-- begin-user-doc -->
  * The <b>Resource </b> associated with the package.
  * <!-- end-user-doc -->
+ * 
  * @see org.eclipse.scada.configuration.world.deployment.util.DeploymentResourceFactoryImpl
  * @generated
  */
@@ -27,12 +27,20 @@ public class DeploymentResourceImpl extends XMIResourceImpl
      * Creates an instance of the resource.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @param uri the URI of the new resource.
+     * 
+     * @param uri
+     *            the URI of the new resource.
      * @generated
      */
-    public DeploymentResourceImpl ( URI uri )
+    public DeploymentResourceImpl ( final URI uri )
     {
         super ( uri );
+    }
+
+    @Override
+    protected boolean useUUIDs ()
+    {
+        return true;
     }
 
 } //DeploymentResourceImpl
