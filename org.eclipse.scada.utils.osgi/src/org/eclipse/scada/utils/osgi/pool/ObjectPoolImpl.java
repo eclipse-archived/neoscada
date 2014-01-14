21894e982d168008664e7ab018c0234d4c25e182
Juergen Rose <juergen.rose@ibh-systems.com>
2014-01-14 12:44:09 +0100

    fixed license header

--- org.eclipse.scada.utils.osgi/src/org/eclipse/scada/utils/osgi/pool/ObjectPoolImpl.java
+++ org.eclipse.scada.utils.osgi/src/org/eclipse/scada/utils/osgi/pool/ObjectPoolImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2012 TH4 SYSTEMS GmbH and others.
+ * Copyright (c) 2006, 2012, 2014 TH4 SYSTEMS GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     TH4 SYSTEMS GmbH - initial API and implementation
+ *     IBH SYSTEMS GmbH - some changes
  *******************************************************************************/
 package org.eclipse.scada.utils.osgi.pool;
 
