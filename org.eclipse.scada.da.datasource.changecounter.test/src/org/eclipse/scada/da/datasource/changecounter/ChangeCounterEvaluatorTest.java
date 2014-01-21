4bb5fe725750f64849b381bc38a1e779c573f11f
Juergen Rose <juergen.rose@ibh-systems.com>
2014-01-21 13:00:39 +0100

    added some more tests

--- org.eclipse.scada.da.datasource.changecounter.test/src/org/eclipse/scada/da/datasource/changecounter/ChangeCounterEvaluatorTest.java
+++ org.eclipse.scada.da.datasource.changecounter.test/src/org/eclipse/scada/da/datasource/changecounter/ChangeCounterEvaluatorTest.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2014 IBH SYSTEMS GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBH SYSTEMS GmbH - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.scada.da.datasource.changecounter;
 
 import static org.junit.Assert.assertEquals;
@@ -20,7 +30,8 @@ public class ChangeCounterEvaluatorTest
         List<Variant> values = new ArrayList<Variant> ();
         values.add ( Variant.valueOf ( 1 ) );
         DataItemValueRange valueRange = new DataItemValueRange ( 60 * 1000 );
-        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 1 ), SubscriptionState.CONNECTED, System.currentTimeMillis () - 66 * 1000, false, false ) );
+        long t = System.currentTimeMillis ();
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 1 ), SubscriptionState.CONNECTED, t - 66 * 1000, false, false ) );
         valueRange.checkRange ();
         assertEquals ( 0, ChangeCounterEvaluator.handleDelta ( values, valueRange.getState (), ErrorHandling.ERROR ) );
     }
@@ -31,8 +42,9 @@ public class ChangeCounterEvaluatorTest
         List<Variant> values = new ArrayList<Variant> ();
         values.add ( Variant.valueOf ( 1 ) );
         DataItemValueRange valueRange = new DataItemValueRange ( 60 * 1000 );
-        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 1 ), SubscriptionState.CONNECTED, System.currentTimeMillis () - 66 * 1000, false, false ) );
-        valueRange.add ( new DataItemValueLight ( Variant.NULL, SubscriptionState.CONNECTED, System.currentTimeMillis (), false, true ) );
+        long t = System.currentTimeMillis ();
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 1 ), SubscriptionState.CONNECTED, t - 66 * 1000, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.NULL, SubscriptionState.CONNECTED, t, false, true ) );
         valueRange.checkRange ();
         ChangeCounterEvaluator.handleDelta ( values, valueRange.getState (), ErrorHandling.ERROR );
     }
@@ -43,8 +55,9 @@ public class ChangeCounterEvaluatorTest
         List<Variant> values = new ArrayList<Variant> ();
         values.add ( Variant.valueOf ( 1 ) );
         DataItemValueRange valueRange = new DataItemValueRange ( 60 * 1000 );
-        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 1 ), SubscriptionState.CONNECTED, System.currentTimeMillis () - 66 * 1000, false, false ) );
-        valueRange.add ( new DataItemValueLight ( Variant.NULL, SubscriptionState.CONNECTED, System.currentTimeMillis (), false, true ) );
+        long t = System.currentTimeMillis ();
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 1 ), SubscriptionState.CONNECTED, t - 66 * 1000, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.NULL, SubscriptionState.CONNECTED, t, false, true ) );
         valueRange.checkRange ();
         assertEquals ( 0, ChangeCounterEvaluator.handleDelta ( values, valueRange.getState (), ErrorHandling.IGNORE ) );
     }
@@ -55,9 +68,127 @@ public class ChangeCounterEvaluatorTest
         List<Variant> values = new ArrayList<Variant> ();
         values.add ( Variant.valueOf ( 1 ) );
         DataItemValueRange valueRange = new DataItemValueRange ( 60 * 1000 );
-        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 1 ), SubscriptionState.CONNECTED, System.currentTimeMillis () - 66 * 1000, false, false ) );
-        valueRange.add ( new DataItemValueLight ( Variant.NULL, SubscriptionState.CONNECTED, System.currentTimeMillis (), false, true ) );
+        long t = System.currentTimeMillis ();
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 1 ), SubscriptionState.CONNECTED, t - 66 * 1000, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.NULL, SubscriptionState.CONNECTED, t, false, true ) );
         valueRange.checkRange ();
         assertEquals ( 1, ChangeCounterEvaluator.handleDelta ( values, valueRange.getState (), ErrorHandling.COUNT ) );
+    }
+
+    @Test
+    public void testHandleSetNoChanges ()
+    {
+        List<Variant> values = new ArrayList<Variant> ();
+        values.add ( Variant.valueOf ( 1 ) );
+        DataItemValueRange valueRange = new DataItemValueRange ( 60 * 1000 );
+        long t = System.currentTimeMillis ();
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 1 ), SubscriptionState.CONNECTED, t - 66 * 1000, false, false ) );
+        valueRange.checkRange ();
+        assertEquals ( 0, ChangeCounterEvaluator.handleSet ( values, valueRange.getState (), ErrorHandling.ERROR ) );
+    }
+
+    @Test ( expected = IllegalArgumentException.class )
+    public void testHandleSetWithError ()
+    {
+        List<Variant> values = new ArrayList<Variant> ();
+        values.add ( Variant.valueOf ( 1 ) );
+        DataItemValueRange valueRange = new DataItemValueRange ( 60 * 1000 );
+        long t = System.currentTimeMillis ();
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 1 ), SubscriptionState.CONNECTED, t - 66 * 1000, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.NULL, SubscriptionState.CONNECTED, t, false, true ) );
+        valueRange.checkRange ();
+        ChangeCounterEvaluator.handleSet ( values, valueRange.getState (), ErrorHandling.ERROR );
+    }
+
+    @Test
+    public void testHandleSetWithIgnoredError ()
+    {
+        List<Variant> values = new ArrayList<Variant> ();
+        values.add ( Variant.valueOf ( 1 ) );
+        DataItemValueRange valueRange = new DataItemValueRange ( 60 * 1000 );
+        long t = System.currentTimeMillis ();
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 1 ), SubscriptionState.CONNECTED, t - 66 * 1000, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.NULL, SubscriptionState.CONNECTED, t, false, true ) );
+        valueRange.checkRange ();
+        assertEquals ( 0, ChangeCounterEvaluator.handleSet ( values, valueRange.getState (), ErrorHandling.IGNORE ) );
+    }
+
+    @Test
+    public void testHandleSetWithCountedError ()
+    {
+        List<Variant> values = new ArrayList<Variant> ();
+        values.add ( Variant.valueOf ( 1 ) );
+        DataItemValueRange valueRange = new DataItemValueRange ( 60 * 1000 );
+        long t = System.currentTimeMillis ();
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 1 ), SubscriptionState.CONNECTED, t - 66 * 1000, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.NULL, SubscriptionState.CONNECTED, t, false, true ) );
+        valueRange.checkRange ();
+        assertEquals ( 1, ChangeCounterEvaluator.handleSet ( values, valueRange.getState (), ErrorHandling.COUNT ) );
+    }
+
+    @Test
+    public void testHandleSetSameList ()
+    {
+        List<Variant> values = new ArrayList<Variant> ();
+        values.add ( Variant.valueOf ( 1 ) );
+        values.add ( Variant.valueOf ( 2 ) );
+        DataItemValueRange valueRange = new DataItemValueRange ( 60 * 1000 );
+        long t = System.currentTimeMillis ();
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 1 ), SubscriptionState.CONNECTED, t - 66 * 1000, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 2 ), SubscriptionState.CONNECTED, t, false, false ) );
+        valueRange.checkRange ();
+        assertEquals ( 0, ChangeCounterEvaluator.handleSet ( values, valueRange.getState (), ErrorHandling.ERROR ) );
+    }
+
+    @Test
+    public void testHandleSet1Change ()
+    {
+        List<Variant> values = new ArrayList<Variant> ();
+        values.add ( Variant.valueOf ( 1 ) );
+        values.add ( Variant.valueOf ( 2 ) );
+        DataItemValueRange valueRange = new DataItemValueRange ( 60 * 1000 );
+        long t = System.currentTimeMillis ();
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 1 ), SubscriptionState.CONNECTED, t - 66 * 1000, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 2 ), SubscriptionState.CONNECTED, t, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 3 ), SubscriptionState.CONNECTED, t + 1, false, false ) );
+        valueRange.checkRange ();
+        assertEquals ( 1, ChangeCounterEvaluator.handleSet ( values, valueRange.getState (), ErrorHandling.ERROR ) );
+    }
+
+    @Test
+    public void testHandleSet2Change ()
+    {
+        List<Variant> values = new ArrayList<Variant> ();
+        values.add ( Variant.valueOf ( 1 ) );
+        values.add ( Variant.valueOf ( 2 ) );
+        DataItemValueRange valueRange = new DataItemValueRange ( 60 * 1000 );
+        long t = System.currentTimeMillis ();
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 1 ), SubscriptionState.CONNECTED, t - 66 * 1000, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 2 ), SubscriptionState.CONNECTED, t, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 3 ), SubscriptionState.CONNECTED, t + 1, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 1 ), SubscriptionState.CONNECTED, t + 2, false, false ) );
+        valueRange.checkRange ();
+        assertEquals ( 2, ChangeCounterEvaluator.handleSet ( values, valueRange.getState (), ErrorHandling.ERROR ) );
+    }
+
+
+    @Test
+    public void testHandleSet3Change ()
+    {
+        List<Variant> values = new ArrayList<Variant> ();
+        values.add ( Variant.valueOf ( 1 ) );
+        values.add ( Variant.valueOf ( 2 ) );
+        DataItemValueRange valueRange = new DataItemValueRange ( 60 * 1000 );
+        long t = System.currentTimeMillis ();
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 1 ), SubscriptionState.CONNECTED, t - 66 * 1000, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 2 ), SubscriptionState.CONNECTED, t, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 3 ), SubscriptionState.CONNECTED, t + 1, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 1 ), SubscriptionState.CONNECTED, t + 2, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 2 ), SubscriptionState.CONNECTED, t + 3, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 2 ), SubscriptionState.CONNECTED, t + 4, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 2 ), SubscriptionState.CONNECTED, t + 5, false, false ) );
+        valueRange.add ( new DataItemValueLight ( Variant.valueOf ( 7 ), SubscriptionState.CONNECTED, t + 6, false, false ) );
+        valueRange.checkRange ();
+        assertEquals ( 3, ChangeCounterEvaluator.handleSet ( values, valueRange.getState (), ErrorHandling.ERROR ) );
     }
 }
