4152285e4bc21bbb242c37ffefcc59df6302284a
Jens Reimann <jens.reimann@ibh-systems.com>
2014-02-28 10:26:39 +0100

    remove the fragment again

--- org.eclipse.scada.utils.osgi.daemon/src/org/eclipse/scada/utils/osgi/daemon/EclipseDaemon.java
+++ org.eclipse.scada.utils.osgi.daemon/src/org/eclipse/scada/utils/osgi/daemon/EclipseDaemon.java
@@ -8,18 +8,25 @@
  * Contributors:
  *     IBH SYSTEMS GmbH - initial API and implementation
  *******************************************************************************/
-package org.eclipse.core.runtime.adaptor;
+package org.eclipse.scada.utils.osgi.daemon;
+
+import org.eclipse.core.runtime.adaptor.EclipseStarter;
 
 /**
  * This is a simple wrapper class for the {@link EclipseStarter} <br/>
  * The problem is that the "prunsrv.exe" of Apache Commons Daemon expects the
  * stop method to also accept arguments of type String[].
  * <p>
- * Also see https://issues.apache.org/jira/browse/DAEMON-315
+ * Also see
+ * <ul>
+ * <li>https://issues.apache.org/jira/browse/DAEMON-315</li>
+ * <li>https://bugs.eclipse.org/bugs/show_bug.cgi?id=429293</li>
+ * </ul>
  * </p>
  * 
  * @author Jens Reimann
  */
+@SuppressWarnings ( "restriction" )
 public class EclipseDaemon
 {
     public static void main ( final String[] args ) throws Exception
