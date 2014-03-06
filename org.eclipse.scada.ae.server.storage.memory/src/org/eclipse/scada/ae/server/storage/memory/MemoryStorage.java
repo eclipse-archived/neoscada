bb30207dd04841d6023083f28c44388650479f59
Jens Reimann <jens.reimann@ibh-systems.com>
2014-03-06 10:55:58 +0100

    fix possible NPE

--- org.eclipse.scada.ae.server.storage.memory/src/org/eclipse/scada/ae/server/storage/memory/MemoryStorage.java
+++ org.eclipse.scada.ae.server.storage.memory/src/org/eclipse/scada/ae/server/storage/memory/MemoryStorage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2012 TH4 SYSTEMS GmbH and others.
+ * Copyright (c) 2009, 2014 TH4 SYSTEMS GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     TH4 SYSTEMS GmbH - initial API and implementation
+ *     IBH SYSTEMS GmbH - bug fix, possible NPE
  *******************************************************************************/
 package org.eclipse.scada.ae.server.storage.memory;
 
@@ -50,7 +51,10 @@ public class MemoryStorage extends BaseStorage
         {
             this.events.add ( storedEvent );
         }
-        listener.notify ( storedEvent );
+        if ( listener != null )
+        {
+            listener.notify ( storedEvent );
+        }
         return storedEvent;
     }
 
