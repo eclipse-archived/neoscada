55af9ff2249ef8b66aa5cf5f1011bef63fc64fd4
Jens Reimann <jens.reimann@ibh-systems.com>
2014-03-20 14:57:39 +0100

    fix boolean logic error

--- org.eclipse.scada.da.server.exporter.common/src/org/eclipse/scada/da/server/exporter/common/AbstractSubscriptionManager.java
+++ org.eclipse.scada.da.server.exporter.common/src/org/eclipse/scada/da/server/exporter/common/AbstractSubscriptionManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 IBH SYSTEMS GmbH and others.
+ * Copyright (c) 2013, 2014 IBH SYSTEMS GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -281,7 +281,7 @@ public abstract class AbstractSubscriptionManager
     {
         logger.trace ( "Subscribe to - itemId: {}", itemId );
 
-        if ( this.started )
+        if ( !this.started )
         {
             throw new IllegalStateException ( "Items may only be subscribed when manager is started" );
         }
