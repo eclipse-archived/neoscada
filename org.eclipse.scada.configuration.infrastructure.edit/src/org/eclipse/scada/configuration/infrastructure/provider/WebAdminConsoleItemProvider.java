8652d6d827a97d783cdf2438243cfab4820c22f9
Jens Reimann <jens.reimann@ibh-systems.com>
2014-05-27 12:18:14 +0200

    only provide possible values

--- org.eclipse.scada.configuration.infrastructure.edit/src/org/eclipse/scada/configuration/infrastructure/provider/WebAdminConsoleItemProvider.java
+++ org.eclipse.scada.configuration.infrastructure.edit/src/org/eclipse/scada/configuration/infrastructure/provider/WebAdminConsoleItemProvider.java
@@ -16,9 +16,9 @@ import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.common.util.ResourceLocator;
-
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IChildCreationExtender;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
@@ -27,13 +27,18 @@ import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ItemProviderAdapter;
 import org.eclipse.scada.configuration.infrastructure.InfrastructurePackage;
+import org.eclipse.scada.configuration.infrastructure.WebAdminConsole;
 
 /**
- * This is the item provider adapter for a {@link org.eclipse.scada.configuration.infrastructure.WebAdminConsole} object.
+ * This is the item provider adapter for a
+ * {@link org.eclipse.scada.configuration.infrastructure.WebAdminConsole}
+ * object.
  * <!-- begin-user-doc -->
  * <!-- end-user-doc -->
+ * 
  * @generated
  */
 public class WebAdminConsoleItemProvider
@@ -49,9 +54,10 @@ public class WebAdminConsoleItemProvider
      * This constructs an instance from a factory and a notifier.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
-    public WebAdminConsoleItemProvider ( AdapterFactory adapterFactory )
+    public WebAdminConsoleItemProvider ( final AdapterFactory adapterFactory )
     {
         super ( adapterFactory );
     }
@@ -60,51 +66,65 @@ public class WebAdminConsoleItemProvider
      * This returns the property descriptors for the adapted class.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     @Override
-    public List<IItemPropertyDescriptor> getPropertyDescriptors ( Object object )
+    public List<IItemPropertyDescriptor> getPropertyDescriptors ( final Object object )
     {
-        if ( itemPropertyDescriptors == null )
+        if ( this.itemPropertyDescriptors == null )
         {
             super.getPropertyDescriptors ( object );
 
             addHttpServicePropertyDescriptor ( object );
         }
-        return itemPropertyDescriptors;
+        return this.itemPropertyDescriptors;
     }
 
     /**
      * This adds a property descriptor for the Http Service feature.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
+     * 
+     * @generated NOT
      */
-    protected void addHttpServicePropertyDescriptor ( Object object )
+    protected void addHttpServicePropertyDescriptor ( final Object object )
     {
-        itemPropertyDescriptors.add
-                ( createItemPropertyDescriptor
-                ( ( (ComposeableAdapterFactory)adapterFactory ).getRootAdapterFactory (),
+        this.itemPropertyDescriptors.add
+                ( new ItemPropertyDescriptor (
+                        ( (ComposeableAdapterFactory)this.adapterFactory ).getRootAdapterFactory (),
                         getResourceLocator (),
-                        getString ( "_UI_WebAdminConsole_httpService_feature" ), //$NON-NLS-1$
-                        getString ( "_UI_PropertyDescriptor_description", "_UI_WebAdminConsole_httpService_feature", "_UI_WebAdminConsole_type" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                        getString ( "_UI_WebAdminConsole_httpService_feature" ),
+                        getString ( "_UI_PropertyDescriptor_description", "_UI_WebAdminConsole_httpService_feature", "_UI_WebAdminConsole_type" ),
                         InfrastructurePackage.Literals.WEB_ADMIN_CONSOLE__HTTP_SERVICE,
                         true,
                         false,
                         true,
                         null,
                         null,
-                        null ) );
+                        null ) {
+                    @Override
+                    protected Collection<?> getComboBoxObjects ( final Object object )
+                    {
+                        if ( object instanceof WebAdminConsole )
+                        {
+                            final EObject parent = ( (WebAdminConsole)object ).eContainer ();
+                            return EcoreUtil.getObjectsByType ( parent.eContents (), InfrastructurePackage.Literals.HTTP_SERVICE_MODULE );
+                        }
+                        return super.getComboBoxObjects ( object );
+                    }
+                } );
     }
 
     /**
      * This returns WebAdminConsole.gif.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     @Override
-    public Object getImage ( Object object )
+    public Object getImage ( final Object object )
     {
         return overlayImage ( object, getResourceLocator ().getImage ( "full/obj16/WebAdminConsole" ) ); //$NON-NLS-1$
     }
@@ -113,37 +133,43 @@ public class WebAdminConsoleItemProvider
      * This returns the label text for the adapted class.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     @Override
-    public String getText ( Object object )
+    public String getText ( final Object object )
     {
         return getString ( "_UI_WebAdminConsole_type" ); //$NON-NLS-1$
     }
 
     /**
-     * This handles model notifications by calling {@link #updateChildren} to update any cached
-     * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
+     * This handles model notifications by calling {@link #updateChildren} to
+     * update any cached
+     * children and by creating a viewer notification, which it passes to
+     * {@link #fireNotifyChanged}.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     @Override
-    public void notifyChanged ( Notification notification )
+    public void notifyChanged ( final Notification notification )
     {
         updateChildren ( notification );
         super.notifyChanged ( notification );
     }
 
     /**
-     * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
+     * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s
+     * describing the children
      * that can be created under this object.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     @Override
-    protected void collectNewChildDescriptors ( Collection<Object> newChildDescriptors, Object object )
+    protected void collectNewChildDescriptors ( final Collection<Object> newChildDescriptors, final Object object )
     {
         super.collectNewChildDescriptors ( newChildDescriptors, object );
     }
@@ -152,12 +178,13 @@ public class WebAdminConsoleItemProvider
      * Return the resource locator for this item provider's resources.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     @Override
     public ResourceLocator getResourceLocator ()
     {
-        return ( (IChildCreationExtender)adapterFactory ).getResourceLocator ();
+        return ( (IChildCreationExtender)this.adapterFactory ).getResourceLocator ();
     }
 
 }
