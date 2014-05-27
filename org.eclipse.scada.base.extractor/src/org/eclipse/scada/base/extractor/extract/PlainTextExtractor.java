1b44d06db9a0bfc1168763762ad3fbba1990a377
Jens Reimann <jens.reimann@ibh-systems.com>
2014-05-27 17:28:22 +0200

    add PlainTextExtractor, which takes the whole string

---
+++ org.eclipse.scada.base.extractor/src/org/eclipse/scada/base/extractor/extract/PlainTextExtractor.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2014 IBH SYSTEMS GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBH SYSTEMS GmbH - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.scada.base.extractor.extract;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.eclipse.scada.core.Variant;
+
+/**
+ * An extractor that simply takes the whole string
+ */
+public class PlainTextExtractor extends AbstractStringExtractor
+{
+    private final ItemDescriptor value;
+
+    public PlainTextExtractor ( final ItemDescriptor value )
+    {
+        this.value = value;
+    }
+
+    @Override
+    protected Map<ItemDescriptor, ItemValue> processData ( final String data ) throws Exception
+    {
+        return Collections.singletonMap ( this.value, new ItemValue ( Variant.valueOf ( data ), null ) );
+    }
+
+}
