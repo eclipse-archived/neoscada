ff414c2928dda971c72f1fa8e0c698fa79b5affa
Jens Reimann <jens.reimann@ibh-systems.com>
2014-06-13 15:05:04 +0200

    create recipes with profiles and names

--- org.eclipse.scada.configuration.ui.project/src/org/eclipse/scada/configuration/ui/project/create/RecipeBuilder.java
+++ org.eclipse.scada.configuration.ui.project/src/org/eclipse/scada/configuration/ui/project/create/RecipeBuilder.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 IBH SYSTEMS GmbH and others.
+ * Copyright (c) 2013, 2014 IBH SYSTEMS GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,11 +12,14 @@ package org.eclipse.scada.configuration.ui.project.create;
 
 import java.io.IOException;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.scada.configuration.recipe.CaptureOutput;
 import org.eclipse.scada.configuration.recipe.Definition;
 import org.eclipse.scada.configuration.recipe.Execute;
 import org.eclipse.scada.configuration.recipe.MapInput;
+import org.eclipse.scada.configuration.recipe.Profile;
+import org.eclipse.scada.configuration.recipe.PropertyEntry;
 import org.eclipse.scada.configuration.recipe.RecipeFactory;
 import org.eclipse.scada.configuration.recipe.StringInputValue;
 import org.eclipse.scada.configuration.recipe.Task;
@@ -31,29 +34,38 @@ public class RecipeBuilder
 
     public static Definition createDefaultRecipe () throws IOException
     {
-        return createRecipe ( false );
+        return createRecipe ( "Production", false );
     }
 
     public static Definition createIntegrationRecipe () throws IOException
     {
-        return createRecipe ( true );
+        return createRecipe ( "Integration", true );
     }
 
-    protected static Definition createRecipe ( final boolean nodeMappings ) throws IOException
+    protected static Definition createRecipe ( final String name, final boolean nodeMappings ) throws IOException
     {
         final Definition recipe = RecipeFactory.eINSTANCE.createDefinition ();
+        recipe.setName ( name );
+
+        final Profile profile = RecipeFactory.eINSTANCE.createProfile ();
+        profile.setName ( "No deployment actions" );
+
+        addProperty ( profile.getProperties (), "skipRunDeployment", "true" ); //$NON-NLS-1$ //$NON-NLS-2$
+
+        recipe.getProfiles ().add ( profile );
+
         recipe.getImport ().add ( new ModelLoader<Definition> ( Definition.class ).load ( URI.createURI ( "platform:/plugin/org.eclipse.scada.configuration.generator.component/default.recipe" ) ) ); //$NON-NLS-1$
 
         {
             final Task loadTask = createTask ( recipe, 0, "Load Models" );
             {
-                final Execute exec = addExecute ( loadTask, LoadModel.class, "load" ); //$NON-NLS-1$ 
+                final Execute exec = addExecute ( loadTask, LoadModel.class, "load" ); //$NON-NLS-1$
                 createStringInputValue ( exec, "uri", "world.escm" ); //$NON-NLS-1$ //$NON-NLS-2$
                 createOutputCapture ( exec, "model", "componentModel" ); //$NON-NLS-1$ //$NON-NLS-2$
             }
             if ( nodeMappings )
             {
-                final Execute exec = addExecute ( loadTask, LoadModel.class, "load" ); //$NON-NLS-1$ 
+                final Execute exec = addExecute ( loadTask, LoadModel.class, "load" ); //$NON-NLS-1$
                 createStringInputValue ( exec, "uri", "nodeMappings.esdi" ); //$NON-NLS-1$ //$NON-NLS-2$
                 createOutputCapture ( exec, "model", "nodeMappings" ); //$NON-NLS-1$ //$NON-NLS-2$
             }
@@ -61,20 +73,28 @@ public class RecipeBuilder
         if ( nodeMappings )
         {
             final Task mappings = createTask ( recipe, 550_000, "Node Mappings" );
-            addExecute ( mappings, NodeMappingHandler.class, "execute" ); //$NON-NLS-1$ 
+            addExecute ( mappings, NodeMappingHandler.class, "execute" ); //$NON-NLS-1$
         }
         {
             final Task storeTask = createTask ( recipe, Integer.MAX_VALUE, "Store Models" );
 
-            final Execute exec1 = addExecute ( storeTask, StoreModel.class, "store" ); //$NON-NLS-1$ 
+            final Execute exec1 = addExecute ( storeTask, StoreModel.class, "store" ); //$NON-NLS-1$
             createStringInputValue ( exec1, "uri", "result.eswm" ); //$NON-NLS-1$ //$NON-NLS-2$
             createMapInput ( exec1, "worldModel", "model" ); //$NON-NLS-1$ //$NON-NLS-2$
 
-            final Execute exec2 = addExecute ( storeTask, StoreModel.class, "store" ); //$NON-NLS-1$ 
+            final Execute exec2 = addExecute ( storeTask, StoreModel.class, "store" ); //$NON-NLS-1$
             createStringInputValue ( exec2, "uri", "result.globalize" ); //$NON-NLS-1$ //$NON-NLS-2$
             createMapInput ( exec2, "globalizationModel", "model" ); //$NON-NLS-1$ //$NON-NLS-2$
         }
         return recipe;
+    }
+
+    protected static void addProperty ( final EList<PropertyEntry> properties, final String key, final String value )
+    {
+        final PropertyEntry pe = RecipeFactory.eINSTANCE.createPropertyEntry ();
+        pe.setKey ( key );
+        pe.setValue ( value );
+        properties.add ( pe );
     }
 
     private static void createOutputCapture ( final Execute exec, final String localName, final String contextName )
