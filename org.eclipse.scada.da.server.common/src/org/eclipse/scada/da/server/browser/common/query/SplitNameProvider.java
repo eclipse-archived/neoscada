ba6597075633877271ebe7e0cdd5d16e3e87e118
Jens Reimann <jens.reimann@ibh-systems.com>
2014-07-15 09:39:23 +0200

    allow negative offsets

--- org.eclipse.scada.da.server.common/src/org/eclipse/scada/da/server/browser/common/query/SplitNameProvider.java
+++ org.eclipse.scada.da.server.common/src/org/eclipse/scada/da/server/browser/common/query/SplitNameProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 TH4 SYSTEMS GmbH and others.
+ * Copyright (c) 2010, 2014 TH4 SYSTEMS GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     TH4 SYSTEMS GmbH - initial API and implementation
+ *     IBH SYSTEMS GmbH - add negative processing
  *******************************************************************************/
 package org.eclipse.scada.da.server.browser.common.query;
 
@@ -16,6 +17,65 @@ import java.util.regex.Pattern;
 
 import org.eclipse.scada.utils.str.StringHelper;
 
+/**
+ * A name provider which will split the name and re-assemble it
+ * <p>
+ * This name provider will take a name, provided by another name provider, split
+ * it into tokens, by the means of a regular expression. After that it will
+ * selected tokens, based on the
+ * <q>fromStart</q> and
+ * <q>fromEnd</q> positions and re-assemble the string using the provided
+ * delimiter.
+ * </p>
+ * <p>
+ * If
+ * <q>fromStart</q> is positive, tokens before that index will be included.
+ * Otherwise they will be rejected.
+ * </p>
+ * <p>
+ * If
+ * <q>fromEnd</q> is positive, tokens after that index will be included.
+ * Otherwise they will be rejected.
+ * </p>
+ * <p>
+ * Assuming the pattern is
+ * <q>-</q> and the delimiter is
+ * <q></q>.
+ * <table>
+ * <tr>
+ * <th>Name
+ * <th>
+ * <th>fromStart</th>
+ * <th>fromEnd</th>
+ * <th>Result</th>
+ * </tr>
+ * <tr>
+ * <td>1-2-3-4-5</td>
+ * <td>2</td>
+ * <td>0</td>
+ * <td>1.2</td>
+ * </tr>
+ * <tr>
+ * <td>1-2-3-4-5</td>
+ * <td>0</td>
+ * <td>2</td>
+ * <td>4.5</td>
+ * </tr>
+ * <tr>
+ * <td>1-2-3-4-5</td>
+ * <td>-2</td>
+ * <td>0</td>
+ * <td>3.4</td>
+ * </tr>
+ * <tr>
+ * <td>1-2-3-4-5</td>
+ * <td>0</td>
+ * <td>-2</td>
+ * <td>1.2.3</td>
+ * </tr>
+ * </table>
+ * </p>
+ */
 public class SplitNameProvider implements NameProvider
 {
 
@@ -35,7 +95,7 @@ public class SplitNameProvider implements NameProvider
      * The parameters fromStart and fromEnd actually define how much to take
      * from the start and from the end.
      * </p>
-     * 
+     *
      * @param nameProvider
      *            the provider of the name to split
      * @param pattern
@@ -61,7 +121,7 @@ public class SplitNameProvider implements NameProvider
      * Actually calls
      * {@link #SplitNameProvider(NameProvider, Pattern, int, int, String)} with
      * {@link Pattern#compile(String)}
-     * 
+     *
      * @param nameProvider
      *            the provider of the name to split
      * @param pattern
@@ -84,12 +144,37 @@ public class SplitNameProvider implements NameProvider
         final String name = this.nameProvider.getName ( descriptor );
 
         final Collection<String> result = new LinkedList<String> ();
+
         final String[] toks = this.pattern.split ( name );
+
         for ( int i = 0; i < toks.length; i++ )
         {
-            if ( i < this.fromStart || i >= toks.length - this.fromEnd )
+            if ( i < this.fromStart && this.fromStart > 0 )
             {
+                // we are in the start window, and the start is positive
                 result.add ( toks[i] );
+                continue;
+            }
+
+            if ( i >= toks.length - this.fromEnd && this.fromEnd > 0 )
+            {
+                // we are in the end window, and the end is positive
+                result.add ( toks[i] );
+                continue;
+            }
+
+            if ( i >= -this.fromStart && this.fromStart < 0 )
+            {
+                // we are outside the start window, and the start is negative
+                result.add ( toks[i] );
+                continue;
+            }
+
+            if ( i < -this.fromEnd && this.fromEnd < 0 )
+            {
+                // we are outside of the end window, and the end is negative
+                result.add ( toks[i] );
+                continue;
             }
         }
 
