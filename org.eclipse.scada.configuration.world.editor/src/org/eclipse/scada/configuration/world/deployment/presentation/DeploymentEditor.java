8f3a2c7d2111875bcbc8877c906eccc3c747c4e1
Jens Reimann <jens.reimann@ibh-systems.com>
2014-08-20 15:19:10 +0200

    enable manual validation

--- org.eclipse.scada.configuration.world.editor/src/org/eclipse/scada/configuration/world/deployment/presentation/DeploymentEditor.java
+++ org.eclipse.scada.configuration.world.editor/src/org/eclipse/scada/configuration/world/deployment/presentation/DeploymentEditor.java
@@ -126,7 +126,10 @@ import org.eclipse.ui.views.properties.PropertySheet;
 import org.eclipse.ui.views.properties.PropertySheetPage;
 import org.eclipse.scada.configuration.security.provider.SecurityItemProviderAdapterFactory;
 import org.eclipse.scada.configuration.world.deployment.provider.DeploymentItemProviderAdapterFactory;
+import org.eclipse.emf.common.ui.viewer.ColumnViewerInformationControlToolTipSupport;
 import org.eclipse.emf.ecore.provider.EcoreItemProviderAdapterFactory;
+import org.eclipse.emf.edit.ui.provider.DecoratingColumLabelProvider;
+import org.eclipse.emf.edit.ui.provider.DiagnosticDecorator;
 import org.eclipse.scada.da.exec.configuration.provider.ConfigurationItemProviderAdapterFactory;
 import org.eclipse.scada.configuration.world.osgi.profile.provider.ProfileItemProviderAdapterFactory;
 import org.eclipse.scada.configuration.world.osgi.provider.OsgiItemProviderAdapterFactory;
@@ -483,23 +486,33 @@ public class DeploymentEditor extends MultiPageEditorPart implements
 
                     protected Collection<Resource> removedResources = new ArrayList<Resource> ();
 
-                    public boolean visit ( IResourceDelta delta )
+                    public boolean visit ( final IResourceDelta delta )
                     {
                         if ( delta.getResource ().getType () == IResource.FILE )
                         {
                             if ( delta.getKind () == IResourceDelta.REMOVED ||
-                                    delta.getKind () == IResourceDelta.CHANGED && delta.getFlags () != IResourceDelta.MARKERS )
+                                    delta.getKind () == IResourceDelta.CHANGED )
                             {
-                                Resource resource = resourceSet.getResource ( URI.createPlatformResourceURI ( delta.getFullPath ().toString (), true ), false );
+                                final Resource resource = resourceSet.getResource ( URI.createPlatformResourceURI ( delta.getFullPath ().toString (), true ), false );
                                 if ( resource != null )
                                 {
                                     if ( delta.getKind () == IResourceDelta.REMOVED )
                                     {
                                         removedResources.add ( resource );
                                     }
-                                    else if ( !savedResources.remove ( resource ) )
+                                    else
                                     {
-                                        changedResources.add ( resource );
+                                        if ( ( delta.getFlags () & IResourceDelta.MARKERS ) != 0 )
+                                        {
+                                            DiagnosticDecorator.DiagnosticAdapter.update ( resource, markerHelper.getMarkerDiagnostics ( resource, (IFile)delta.getResource () ) );
+                                        }
+                                        if ( ( delta.getFlags () & IResourceDelta.CONTENT ) != 0 )
+                                        {
+                                            if ( !savedResources.remove ( resource ) )
+                                            {
+                                                changedResources.add ( resource );
+                                            }
+                                        }
                                     }
                                 }
                             }
@@ -1148,12 +1161,13 @@ public class DeploymentEditor extends MultiPageEditorPart implements
                 selectionViewer = (TreeViewer)viewerPane.getViewer ();
                 selectionViewer.setContentProvider ( new AdapterFactoryContentProvider ( adapterFactory ) );
 
-                selectionViewer.setLabelProvider ( new AdapterFactoryLabelProvider ( adapterFactory ) );
+                selectionViewer.setLabelProvider ( new DecoratingColumLabelProvider ( new AdapterFactoryLabelProvider ( adapterFactory ), new DiagnosticDecorator ( editingDomain.getResourceSet (), selectionViewer ) ) );
                 selectionViewer.setInput ( editingDomain.getResourceSet () );
                 selectionViewer.setSelection ( new StructuredSelection ( editingDomain.getResourceSet ().getResources ().get ( 0 ) ), true );
                 viewerPane.setTitle ( editingDomain.getResourceSet () );
 
                 new AdapterFactoryTreeEditor ( selectionViewer.getTree (), adapterFactory );
+                new ColumnViewerInformationControlToolTipSupport ( selectionViewer, new DiagnosticDecorator.EditingDomainLocationListener ( editingDomain, selectionViewer ) );
 
                 createContextMenuFor ( selectionViewer );
                 int pageIndex = addPage ( viewerPane.getControl () );
@@ -1244,9 +1258,10 @@ public class DeploymentEditor extends MultiPageEditorPart implements
                 viewerPane.createControl ( getContainer () );
                 treeViewer = (TreeViewer)viewerPane.getViewer ();
                 treeViewer.setContentProvider ( new AdapterFactoryContentProvider ( adapterFactory ) );
-                treeViewer.setLabelProvider ( new AdapterFactoryLabelProvider ( adapterFactory ) );
+                treeViewer.setLabelProvider ( new DecoratingColumLabelProvider ( new AdapterFactoryLabelProvider ( adapterFactory ), new DiagnosticDecorator ( editingDomain.getResourceSet (), treeViewer ) ) );
 
                 new AdapterFactoryTreeEditor ( treeViewer.getTree (), adapterFactory );
+                new ColumnViewerInformationControlToolTipSupport ( treeViewer, new DiagnosticDecorator.EditingDomainLocationListener ( editingDomain, treeViewer ) );
 
                 createContextMenuFor ( treeViewer );
                 int pageIndex = addPage ( viewerPane.getControl () );
@@ -1293,7 +1308,9 @@ public class DeploymentEditor extends MultiPageEditorPart implements
 
                 tableViewer.setColumnProperties ( new String[] { "a", "b" } ); //$NON-NLS-1$ //$NON-NLS-2$
                 tableViewer.setContentProvider ( new AdapterFactoryContentProvider ( adapterFactory ) );
-                tableViewer.setLabelProvider ( new AdapterFactoryLabelProvider ( adapterFactory ) );
+                tableViewer.setLabelProvider ( new DecoratingColumLabelProvider ( new AdapterFactoryLabelProvider ( adapterFactory ), new DiagnosticDecorator ( editingDomain.getResourceSet (), tableViewer ) ) );
+
+                new ColumnViewerInformationControlToolTipSupport ( tableViewer, new DiagnosticDecorator.EditingDomainLocationListener ( editingDomain, tableViewer ) );
 
                 createContextMenuFor ( tableViewer );
                 int pageIndex = addPage ( viewerPane.getControl () );
@@ -1340,7 +1357,9 @@ public class DeploymentEditor extends MultiPageEditorPart implements
 
                 treeViewerWithColumns.setColumnProperties ( new String[] { "a", "b" } ); //$NON-NLS-1$ //$NON-NLS-2$
                 treeViewerWithColumns.setContentProvider ( new AdapterFactoryContentProvider ( adapterFactory ) );
-                treeViewerWithColumns.setLabelProvider ( new AdapterFactoryLabelProvider ( adapterFactory ) );
+                treeViewerWithColumns.setLabelProvider ( new DecoratingColumLabelProvider ( new AdapterFactoryLabelProvider ( adapterFactory ), new DiagnosticDecorator ( editingDomain.getResourceSet (), treeViewerWithColumns ) ) );
+
+                new ColumnViewerInformationControlToolTipSupport ( treeViewerWithColumns, new DiagnosticDecorator.EditingDomainLocationListener ( editingDomain, treeViewerWithColumns ) );
 
                 createContextMenuFor ( treeViewerWithColumns );
                 int pageIndex = addPage ( viewerPane.getControl () );
@@ -1498,8 +1517,10 @@ public class DeploymentEditor extends MultiPageEditorPart implements
                     // Set up the tree viewer.
                     //
                     contentOutlineViewer.setContentProvider ( new AdapterFactoryContentProvider ( adapterFactory ) );
-                    contentOutlineViewer.setLabelProvider ( new AdapterFactoryLabelProvider ( adapterFactory ) );
+                    contentOutlineViewer.setLabelProvider ( new DecoratingColumLabelProvider ( new AdapterFactoryLabelProvider ( adapterFactory ), new DiagnosticDecorator ( editingDomain.getResourceSet (), contentOutlineViewer ) ) );
                     contentOutlineViewer.setInput ( editingDomain.getResourceSet () );
+
+                    new ColumnViewerInformationControlToolTipSupport ( contentOutlineViewer, new DiagnosticDecorator.EditingDomainLocationListener ( editingDomain, contentOutlineViewer ) );
 
                     // Make sure our popups work.
                     //
@@ -1556,7 +1577,7 @@ public class DeploymentEditor extends MultiPageEditorPart implements
     public IPropertySheetPage getPropertySheetPage ()
     {
         PropertySheetPage propertySheetPage =
-                new ExtendedPropertySheetPage ( editingDomain )
+                new ExtendedPropertySheetPage ( editingDomain, ExtendedPropertySheetPage.Decoration.MANUAL )
                 {
                     @Override
                     public void setSelectionToViewer ( List<?> selection )
