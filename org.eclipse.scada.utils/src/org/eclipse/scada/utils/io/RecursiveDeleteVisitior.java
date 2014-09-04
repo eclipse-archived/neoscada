4d32f7d68525d0a91b3f0b87483d71b0142ab14b
Jens Reimann <jens.reimann@ibh-systems.com>
2014-09-04 11:46:54 +0200

    dont make it final

--- org.eclipse.scada.utils/src/org/eclipse/scada/utils/io/RecursiveDeleteVisitior.java
+++ org.eclipse.scada.utils/src/org/eclipse/scada/utils/io/RecursiveDeleteVisitior.java
@@ -17,7 +17,7 @@ import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
-public final class RecursiveDeleteVisitior extends SimpleFileVisitor<Path>
+public class RecursiveDeleteVisitior extends SimpleFileVisitor<Path>
 {
     @Override
     public FileVisitResult visitFile ( final Path file, final BasicFileAttributes attrs ) throws IOException
