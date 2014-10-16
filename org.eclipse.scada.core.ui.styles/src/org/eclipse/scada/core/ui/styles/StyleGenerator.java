05c8f07cc3d6d097a425879ff824b25443df455b
Jens Reimann <jens.reimann@ibh-systems.com>
2014-10-16 09:35:07 +0200

    add docs

--- org.eclipse.scada.core.ui.styles/src/org/eclipse/scada/core/ui/styles/StyleGenerator.java
+++ org.eclipse.scada.core.ui.styles/src/org/eclipse/scada/core/ui/styles/StyleGenerator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2013 TH4 SYSTEMS GmbH and others.
+ * Copyright (c) 2012, 2014 TH4 SYSTEMS GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,9 +8,19 @@
  * Contributors:
  *     TH4 SYSTEMS GmbH - initial API and implementation
  *     Jens Reimann - additional work
+ *     IBH SYSTEMS GmbH - document
  *******************************************************************************/
 package org.eclipse.scada.core.ui.styles;
 
+/**
+ * A generator for style information based in state information <br/>
+ * The main purpose of a style generator is to create style information
+ * based on a current state. The main method for this is
+ * {@link #generateStyle(StateInformation)}. <br/>
+ * Style generators only provide the information about the styles to use, but do
+ * not apply them. You can either do this on your own or use a class like
+ * {@link StyleHandler}.
+ */
 public interface StyleGenerator
 {
     public interface GeneratorListener
