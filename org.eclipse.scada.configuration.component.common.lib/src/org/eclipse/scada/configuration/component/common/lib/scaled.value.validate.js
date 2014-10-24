451204a4e174f37bf25616c879ad87b2ffdb520f
Juergen Rose <juergen.rose@ibh-systems.com>
2014-10-24 13:17:10 +0200

    added ability to deal with min > max, also improved error message

--- org.eclipse.scada.configuration.component.common.lib/src/org/eclipse/scada/configuration/component/common/lib/scaled.value.validate.js
+++ org.eclipse.scada.configuration.component.common.lib/src/org/eclipse/scada/configuration/component/common/lib/scaled.value.validate.js
@@ -1,3 +1,17 @@
-if ( @@value@@ < @@inMin@@ || @@value@@ > @@inMax@@ ) {
-   throw "Out of range";
+if ( @@inMin@@ > @@inMax@@ ) {
+	var r = "MIN(@@inMin@@) > MAX(@@inMax@@)";
+	if ( @@value@@ > @@inMin@@) {
+	   throw "Out of range (" + r + "): @@value@@ = " + @@value@@ + "; @@value@@ > @@inMin@@";
+	}
+	if ( @@value@@ < @@inMax@@) {
+		   throw "Out of range (" + r + "): @@value@@ = " + @@value@@ + "; @@value@@ < @@inMax@@";
+	}
+} else {
+	var r = "MIN(@@inMin@@) < MAX(@@inMax@@)";
+	if ( @@value@@ > @@inMax@@) {
+	   throw "Out of range (" + r + "): @@value@@ = " + @@value@@ + "; @@value@@ > @@inMax@@";
+	}
+	if ( @@value@@ < @@inMin@@) {
+	   throw "Out of range (" + r + "): @@value@@ = " + @@value@@ + "; @@value@@ < @@inMin@@";
+	}
 }
