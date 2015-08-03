006793e77417576ccd77ba0c97c838a3f030d9ab
Jens Reimann <jens.reimann@ibh-systems.com>
2015-08-03 15:36:43 +0200

    automatically process merges if required

---
+++ org.eclipse.scada.ca.updater/src/org/eclipse/scada/ca/updater/internal/UpdatingMergeWatcher.java
@@ -0,0 +1,93 @@
+/*******************************************************************************
+ * Copyright (c) 2015 IBH SYSTEMS GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBH SYSTEMS GmbH - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.scada.ca.updater.internal;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.scada.ca.ConfigurationAdministrator;
+import org.eclipse.scada.ca.updater.Updater;
+import org.eclipse.scada.utils.concurrent.NotifyFuture;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UpdatingMergeWatcher extends AbstractMergeWatcher
+{
+    private final static Logger logger = LoggerFactory.getLogger ( UpdatingMergeWatcher.class );
+
+    private final ConfigurationAdministrator admin;
+
+    private final Path path;
+
+    private final boolean watchFile;
+
+    public UpdatingMergeWatcher ( final ConfigurationAdministrator admin, final Path path, final long delay, final TimeUnit timeUnit ) throws IOException
+    {
+        super ( path, delay, timeUnit );
+        this.path = path;
+        this.admin = admin;
+
+        this.watchFile = Files.isRegularFile ( path );
+
+        // initial update
+        processFireChange ();
+    }
+
+    @Override
+    protected void processFireChange ()
+    {
+        logger.info ( "Process change - merging ..." );
+
+        try
+        {
+            final Updater updater = new Updater ( this.admin );
+
+            if ( this.watchFile )
+            {
+                try ( Reader reader = Files.newBufferedReader ( this.path, StandardCharsets.UTF_8 ) )
+                {
+                    updater.loadJson ( reader );
+                }
+            }
+            else
+            {
+                // FIXME: implement directory mode
+            }
+
+            final int changes = updater.getChanges ().size ();
+
+            if ( changes <= 0 )
+            {
+                logger.info ( "No changes ... skipping!" );
+                return;
+            }
+
+            logger.info ( "Starting update ... {} changes", changes );
+            final NotifyFuture<Void> future = updater.update ();
+            future.addListener ( new Runnable () {
+                @Override
+                public void run ()
+                {
+                    logger.info ( "Update complete" );
+                }
+            } );
+        }
+        catch ( final Exception e )
+        {
+            logger.warn ( "Failed to perform merge", e );
+        }
+    }
+
+}
