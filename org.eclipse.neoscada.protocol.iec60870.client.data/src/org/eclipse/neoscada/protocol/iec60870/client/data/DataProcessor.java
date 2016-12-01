a4bd01568fd8fca39433766153965fa222d237f1
Jens Reimann <jreimann@redhat.com>
2016-12-01 17:54:04 +0100

    Refactor to have a common base class for processing

--- org.eclipse.neoscada.protocol.iec60870.client.data/src/org/eclipse/neoscada/protocol/iec60870/client/data/DataProcessor.java
+++ org.eclipse.neoscada.protocol.iec60870.client.data/src/org/eclipse/neoscada/protocol/iec60870/client/data/DataProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 IBH SYSTEMS GmbH and others.
+ * Copyright (c) 2014, 2016 IBH SYSTEMS GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,33 +7,20 @@
  *
  * Contributors:
  *     IBH SYSTEMS GmbH - initial API and implementation
+ *     Red Hat Inc - refactoring
  *******************************************************************************/
 package org.eclipse.neoscada.protocol.iec60870.client.data;
 
 import java.util.concurrent.Executor;
 
-import org.eclipse.neoscada.protocol.iec60870.asdu.message.DoublePointInformationSequence;
-import org.eclipse.neoscada.protocol.iec60870.asdu.message.DoublePointInformationSingle;
-import org.eclipse.neoscada.protocol.iec60870.asdu.message.DoublePointInformationTimeSingle;
-import org.eclipse.neoscada.protocol.iec60870.asdu.message.MeasuredValueScaledSequence;
-import org.eclipse.neoscada.protocol.iec60870.asdu.message.MeasuredValueScaledSingle;
-import org.eclipse.neoscada.protocol.iec60870.asdu.message.MeasuredValueScaledTimeSingle;
-import org.eclipse.neoscada.protocol.iec60870.asdu.message.MeasuredValueShortFloatingPointSequence;
-import org.eclipse.neoscada.protocol.iec60870.asdu.message.MeasuredValueShortFloatingPointSingle;
-import org.eclipse.neoscada.protocol.iec60870.asdu.message.MeasuredValueShortFloatingPointTimeSingle;
-import org.eclipse.neoscada.protocol.iec60870.asdu.message.SinglePointInformationSequence;
-import org.eclipse.neoscada.protocol.iec60870.asdu.message.SinglePointInformationSingle;
-import org.eclipse.neoscada.protocol.iec60870.asdu.message.SinglePointInformationTimeSingle;
 import org.eclipse.neoscada.protocol.iec60870.asdu.types.ASDUAddress;
-import org.eclipse.neoscada.protocol.iec60870.asdu.types.DoublePoint;
-import org.eclipse.neoscada.protocol.iec60870.asdu.types.InformationEntry;
 import org.eclipse.neoscada.protocol.iec60870.asdu.types.InformationObjectAddress;
 import org.eclipse.neoscada.protocol.iec60870.asdu.types.QualifierOfInterrogation;
 import org.eclipse.neoscada.protocol.iec60870.asdu.types.Value;
 
 import io.netty.channel.ChannelHandlerContext;
 
-public class DataProcessor implements DataHandler
+public class DataProcessor extends AbstractDataProcessor
 {
     private final DataListener listener;
 
@@ -76,6 +63,7 @@ public class DataProcessor implements DataHandler
         } );
     }
 
+    @Override
     protected void fireEntry ( final ASDUAddress asduAddress, final InformationObjectAddress address, final Value<?> value )
     {
         this.executor.execute ( new Runnable () {
@@ -86,126 +74,6 @@ public class DataProcessor implements DataHandler
                 DataProcessor.this.listener.update ( asduAddress, address, value );
             }
         } );
-    }
-
-    @Override
-    public void process ( final SinglePointInformationTimeSingle msg )
-    {
-        for ( final InformationEntry<Boolean> entry : msg.getEntries () )
-        {
-            fireEntry ( msg.getHeader ().getAsduAddress (), entry.getAddress (), entry.getValue () );
-        }
-    }
-
-    @Override
-    public void process ( final SinglePointInformationSingle msg )
-    {
-        for ( final InformationEntry<Boolean> entry : msg.getEntries () )
-        {
-            fireEntry ( msg.getHeader ().getAsduAddress (), entry.getAddress (), entry.getValue () );
-        }
-    }
-
-    @Override
-    public void process ( final SinglePointInformationSequence msg )
-    {
-        int i = msg.getStartAddress ().getAddress ();
-
-        for ( final Value<Boolean> value : msg.getValues () )
-        {
-            fireEntry ( msg.getHeader ().getAsduAddress (), InformationObjectAddress.valueOf ( i ), value );
-            i++;
-        }
-    }
-
-    @Override
-    public void process ( final DoublePointInformationTimeSingle msg )
-    {
-        for ( final InformationEntry<DoublePoint> entry : msg.getEntries () )
-        {
-            fireEntry ( msg.getHeader ().getAsduAddress (), entry.getAddress (), entry.getValue () );
-        }
-    }
-
-    @Override
-    public void process ( final DoublePointInformationSingle msg )
-    {
-        for ( final InformationEntry<DoublePoint> entry : msg.getEntries () )
-        {
-            fireEntry ( msg.getHeader ().getAsduAddress (), entry.getAddress (), entry.getValue () );
-        }
-    }
-
-    @Override
-    public void process ( final DoublePointInformationSequence msg )
-    {
-        int i = msg.getStartAddress ().getAddress ();
-
-        for ( final Value<DoublePoint> value : msg.getValues () )
-        {
-            fireEntry ( msg.getHeader ().getAsduAddress (), InformationObjectAddress.valueOf ( i ), value );
-            i++;
-        }
-    }
-
-    @Override
-    public void process ( final MeasuredValueShortFloatingPointTimeSingle msg )
-    {
-        for ( final InformationEntry<Float> entry : msg.getEntries () )
-        {
-            fireEntry ( msg.getHeader ().getAsduAddress (), entry.getAddress (), entry.getValue () );
-        }
-    }
-
-    @Override
-    public void process ( final MeasuredValueShortFloatingPointSingle msg )
-    {
-        for ( final InformationEntry<Float> entry : msg.getEntries () )
-        {
-            fireEntry ( msg.getHeader ().getAsduAddress (), entry.getAddress (), entry.getValue () );
-        }
-    }
-
-    @Override
-    public void process ( final MeasuredValueShortFloatingPointSequence msg )
-    {
-        int i = msg.getStartAddress ().getAddress ();
-
-        for ( final Value<Float> value : msg.getValues () )
-        {
-            fireEntry ( msg.getHeader ().getAsduAddress (), InformationObjectAddress.valueOf ( i ), value );
-            i++;
-        }
-    }
-
-    @Override
-    public void process ( final MeasuredValueScaledTimeSingle msg )
-    {
-        for ( final InformationEntry<Short> entry : msg.getEntries () )
-        {
-            fireEntry ( msg.getHeader ().getAsduAddress (), entry.getAddress (), entry.getValue () );
-        }
-    }
-
-    @Override
-    public void process ( final MeasuredValueScaledSingle msg )
-    {
-        for ( final InformationEntry<Short> entry : msg.getEntries () )
-        {
-            fireEntry ( msg.getHeader ().getAsduAddress (), entry.getAddress (), entry.getValue () );
-        }
-    }
-
-    @Override
-    public void process ( final MeasuredValueScaledSequence msg )
-    {
-        int i = msg.getStartAddress ().getAddress ();
-
-        for ( final Value<Short> value : msg.getValues () )
-        {
-            fireEntry ( msg.getHeader ().getAsduAddress (), InformationObjectAddress.valueOf ( i ), value );
-            i++;
-        }
     }
 
 }
