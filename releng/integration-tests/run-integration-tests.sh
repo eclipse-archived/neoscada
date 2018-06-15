300fb82914d3300af998459f539b501a767460b9
Juergen Rose <juergen.rose@ibh-systems.com>
2018-06-15 21:09:57 +0200

    added ubuntu 18.04
    Signed-off-by: Juergen Rose <juergen.rose@ibh-systems.com>

--- releng/integration-tests/run-integration-tests.sh
+++ releng/integration-tests/run-integration-tests.sh
@@ -8,17 +8,23 @@ fi
 # ---------------------------------------------------------------
 # build a base image to improve runtime of integration tests
 # ---------------------------------------------------------------
-docker build -t neoscada:it-centos73-base ./centos7.3/base
-docker run neoscada:it-centos73-base
+#docker build -t neoscada:it-centos73-base ./centos7.3/base
+#docker run neoscada:it-centos73-base
 
-docker build -t neoscada:it-opensuse422-base ./opensuse42.2/base 
-docker run neoscada:it-opensuse422-base
+#docker build -t neoscada:it-opensuse422-base ./opensuse42.2/base 
+#docker run neoscada:it-opensuse422-base
 
-docker build -t neoscada:it-ubuntu14045-base ./ubuntu1404.5/base 
-docker run neoscada:it-ubuntu14045-base
+#docker build -t neoscada:it-ubuntu14045-base ./ubuntu1404.5/base 
+#docker run neoscada:it-ubuntu14045-base
 
-docker build -t neoscada:it-ubuntu16042-base ./ubuntu1604.2/base 
-docker run neoscada:it-ubuntu16042-base
+#docker build -t neoscada:it-ubuntu16042-base ./ubuntu1604.2/base 
+#docker run neoscada:it-ubuntu16042-base
+
+docker build -t neoscada:it-ubuntu18040-java9-base ./ubuntu1804.0-java9/base 
+docker run neoscada:it-ubuntu18040-java9-base
+
+#docker build -t neoscada:it-ubuntu18040-java10-base ./ubuntu1804.0-java10/base 
+#docker run neoscada:it-ubuntu18040-java10-base
 
 # ---------------------------------------------------------------
 # assemble installation files
@@ -27,6 +33,8 @@ rm -rf centos7.3/neoscada/install
 rm -rf opensuse42.2/neoscada/install
 rm -rf ubuntu1404.5/neoscada/install
 rm -rf ubuntu1604.2/neoscada/install
+rm -rf ubuntu1804.0-java9/neoscada/install
+rm -rf ubuntu1804.0-java10/neoscada/install
 
 rm -rf install
 
@@ -36,6 +44,8 @@ mkdir -p centos7.3/neoscada/install
 mkdir -p opensuse42.2/neoscada/install
 mkdir -p ubuntu1404.5/neoscada/install
 mkdir -p ubuntu1604.2/neoscada/install
+mkdir -p ubuntu1804.0-java9/neoscada/install
+mkdir -p ubuntu1804.0-java10/neoscada/install
 
 cp ../../deploy/org.eclipse.scada.deploy.repack/target/*.deb install/
 cp ../../deploy/org.eclipse.scada.deploy.repack/target/*.rpm install/
@@ -63,16 +73,22 @@ cp install/*.rpm centos7.3/neoscada/install
 cp install/*.rpm opensuse42.2/neoscada/install
 cp install/*.deb ubuntu1404.5/neoscada/install
 cp install/*.deb ubuntu1604.2/neoscada/install
+cp install/*.deb ubuntu1804.0-java9/neoscada/install
+cp install/*.deb ubuntu1804.0-java10/neoscada/install
 
 cp common/* centos7.3/neoscada/install
 cp common/* opensuse42.2/neoscada/install
 cp common/* ubuntu1404.5/neoscada/install
 cp common/* ubuntu1604.2/neoscada/install
+cp common/* ubuntu1804.0-java9/neoscada/install
+cp common/* ubuntu1804.0-java10/neoscada/install
 
 cp configurations/results/rpmbased*.rpm centos7.3/neoscada/install
 cp configurations/results/rpmbased*.rpm opensuse42.2/neoscada/install
 cp configurations/results/ubuntu-upstart*.deb ubuntu1404.5/neoscada/install
 cp configurations/results/ubuntu-systemd*.deb ubuntu1604.2/neoscada/install
+cp configurations/results/ubuntu-systemd*.deb ubuntu1804.0-java9/neoscada/install
+cp configurations/results/ubuntu-systemd*.deb ubuntu1804.0-java10/neoscada/install
 
 # ---------------------------------------------------------------
 # install and run integration tests
@@ -83,8 +99,11 @@ cp configurations/results/ubuntu-systemd*.deb ubuntu1604.2/neoscada/install
 #docker build -t neoscada:it-opensuse422-test1 ./opensuse42.2/neoscada 
 #docker run neoscada:it-opensuse422-test1
 
-docker build -t neoscada:it-ubuntu14045-test1 ./ubuntu1404.5/neoscada 
-docker run neoscada:it-ubuntu14045-test1
+#docker build -t neoscada:it-ubuntu14045-test1 ./ubuntu1404.5/neoscada 
+#docker run neoscada:it-ubuntu14045-test1
 
 #docker build -t neoscada:it-ubuntu16042-test1 ./ubuntu1604.2/neoscada 
 #docker run neoscada:it-ubuntu16042-test1
+
+docker build -t neoscada:it-ubuntu18040-java9-test1 ./ubuntu1804.0-java9/neoscada 
+docker run neoscada:it-ubuntu18040-java9-test1
---
+++ releng/integration-tests/ubuntu1804.0-java9/neoscada/Dockerfile
@@ -0,0 +1,21 @@
+FROM neoscada:it-ubuntu18040-java10-base
+MAINTAINER juergen.rose@ibh-systems.com 
+
+ADD install /root/neoscada-install/
+
+RUN dpkg -i /root/neoscada-install/neoscada.common*.deb
+RUN dpkg -i /root/neoscada-install/org.eclipse.platform*.deb
+RUN dpkg -i /root/neoscada-install/emf-xsd-Update*.deb
+RUN dpkg -i /root/neoscada-install/equinox-sdk*.deb
+RUN dpkg -i /root/neoscada-install/gemini-dbaccess*.deb
+RUN dpkg -i /root/neoscada-install/neoscada.external.p2*.deb
+RUN dpkg -i /root/neoscada-install/neoscada.utils.p2*.deb
+RUN dpkg -i /root/neoscada-install/neoscada.base.p2*.deb
+RUN dpkg -i /root/neoscada-install/neoscada.core.p2*.deb
+RUN dpkg -i /root/neoscada-install/neoscada.protocols.p2*.deb
+RUN dpkg -i /root/neoscada-install/neoscada.deploy.p2*.deb
+
+RUN dpkg -i /root/neoscada-install/ubuntu-systemd-configuration*.deb
+RUN scada.create.master
+
+CMD /bin/bash /root/neoscada-install/launch-master.sh
