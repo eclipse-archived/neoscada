f2d2b5a8f33392b51bae65d1a5d7b8d14646448b
Juergen Rose <juergen.rose@ibh-systems.com>
2018-06-15 20:29:58 +0200

    for this release we will still support upstart (grudgingly)
    Signed-off-by: Juergen Rose <juergen.rose@ibh-systems.com>

--- releng/integration-tests/run-integration-tests.sh
+++ releng/integration-tests/run-integration-tests.sh
@@ -14,6 +14,9 @@ docker run neoscada:it-centos73-base
 docker build -t neoscada:it-opensuse422-base ./opensuse42.2/base 
 docker run neoscada:it-opensuse422-base
 
+docker build -t neoscada:it-ubuntu14045-base ./ubuntu1404.5/base 
+docker run neoscada:it-ubuntu14045-base
+
 docker build -t neoscada:it-ubuntu16042-base ./ubuntu1604.2/base 
 docker run neoscada:it-ubuntu16042-base
 
@@ -22,6 +25,7 @@ docker run neoscada:it-ubuntu16042-base
 # ---------------------------------------------------------------
 rm -rf centos7.3/neoscada/install
 rm -rf opensuse42.2/neoscada/install
+rm -rf ubuntu1404.5/neoscada/install
 rm -rf ubuntu1604.2/neoscada/install
 
 rm -rf install
@@ -30,6 +34,7 @@ mkdir -p install
 
 mkdir -p centos7.3/neoscada/install
 mkdir -p opensuse42.2/neoscada/install
+mkdir -p ubuntu1404.5/neoscada/install
 mkdir -p ubuntu1604.2/neoscada/install
 
 cp ../../deploy/org.eclipse.scada.deploy.repack/target/*.deb install/
@@ -56,15 +61,18 @@ cp ../../deploy/org.eclipse.scada.deploy-p2/target/*.rpm install/
 
 cp install/*.rpm centos7.3/neoscada/install
 cp install/*.rpm opensuse42.2/neoscada/install
+cp install/*.deb ubuntu1404.5/neoscada/install
 cp install/*.deb ubuntu1604.2/neoscada/install
 
 cp common/* centos7.3/neoscada/install
 cp common/* opensuse42.2/neoscada/install
+cp common/* ubuntu1404.5/neoscada/install
 cp common/* ubuntu1604.2/neoscada/install
 
 cp configurations/results/rpmbased*.rpm centos7.3/neoscada/install
 cp configurations/results/rpmbased*.rpm opensuse42.2/neoscada/install
-cp configurations/results/ubuntu*.deb ubuntu1604.2/neoscada/install
+cp configurations/results/ubuntu-upstart*.deb ubuntu1404.5/neoscada/install
+cp configurations/results/ubuntu-systemd*.deb ubuntu1604.2/neoscada/install
 
 # ---------------------------------------------------------------
 # install and run integration tests
@@ -72,8 +80,11 @@ cp configurations/results/ubuntu*.deb ubuntu1604.2/neoscada/install
 #docker build -t neoscada:it-centos73-test1 ./centos7.3/neoscada
 #docker run neoscada:it-centos73-test1
 
-docker build -t neoscada:it-opensuse422-test1 ./opensuse42.2/neoscada 
-docker run neoscada:it-opensuse422-test1
+#docker build -t neoscada:it-opensuse422-test1 ./opensuse42.2/neoscada 
+#docker run neoscada:it-opensuse422-test1
+
+docker build -t neoscada:it-ubuntu14045-test1 ./ubuntu1404.5/neoscada 
+docker run neoscada:it-ubuntu14045-test1
 
 #docker build -t neoscada:it-ubuntu16042-test1 ./ubuntu1604.2/neoscada 
 #docker run neoscada:it-ubuntu16042-test1
---
+++ releng/integration-tests/ubuntu1404.5/neoscada/README.md
@@ -0,0 +1,4 @@
+All integration tests are based on Linux LTS images released in November 2016
+
+The integration tests will basically install some test configuration projects
+and consist of a bunch of python scripts to verify that 
