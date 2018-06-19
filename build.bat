6da18a91a450e01ae36d9914e566dc1bd958e5cc
Juergen Rose <juergen.rose@ibh-systems.com>
2018-06-19 21:31:41 +0200

    removed double quotes from build.bat [ci skip]
    Signed-off-by: Juergen Rose <juergen.rose@ibh-systems.com>

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-SET MAVEN_OPTS="-Xms1024m -Xmx3072m"
+SET MAVEN_OPTS=-Xms1024m -Xmx3072m
 mvn -B -Dorg.slf4j.simpleLogger.defaultLogLevel=INFO  -Dmaven.test.failure.ignore=true -Dmaven.test.skip=true clean install
